4d5835bdeba4a5a05dc5c78ec2f5deaed00062b5
Bilobra <bra.carrozzo@gmail.com>
2022-08-05 15:26:41 +0200

    no destrutturazione but [ ]

--- main.js
+++ main.js
@@ -60,34 +60,34 @@ const postWrapper = document.getElementById('container')
 console.log(postWrapper)
 
 posts.forEach((post) => {
-    let { content, media,author, name, image, likes, created } = post
+    // let { content, media, author, name, image, likes, created } = post
     postWrapper.innerHTML +=
         `
       <div class="post">
             <div class="post__header">
                 <div class="post-meta">
                     <div class="post-meta__icon">
-                    <img class="profile-pic" src="${image}" >
+                    <img class="profile-pic" src="${post.author[image]}" >
                     </div>
                     <div class="post-meta__data">
-                    <div class="post-meta__author">${name}</div>
-                    <div class="post-meta__time">${created}</div>
+                    <div class="post-meta__author">${post.author[name]}</div>
+                    <div class="post-meta__time">${post.created}</div>
                 </div> 
             </div>
-            <div class="post__text">${content}</div>
+            <div class="post__text">${post.content}</div>
             <div class="post__image">
-                <img src="${media}" alt="">
+                <img src="${post.media}" alt="">
             </div>
             <div class="post__footer">
                 <div class="likes js-likes">
                     <div class="likes__cta">
-                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <a class="like-button  js-like-button" href="#" data-postid="${post.id}">
                                 <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                                 <span class="like-button__label">Mi Piace</span>
                         </a>
                     </div>
                     <div class="likes__counter">
-                            Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+                            Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
                     </div>
                 </div> 
             </div>  
